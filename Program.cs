f98f90c8a211ac135dcbc4a7ee181e3b8609eb17
HEVNz <e-exe-e@live.com>
2022-05-19 22:35:20 +0700

    g

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnetcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test();
+        }
+
+        public static void Test()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+}
